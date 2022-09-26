7c14435c7336bcfb2de5cbb8e5b5d9146132ff77
Joseph Roper <1901881@uad.ac.uk>
2022-09-27 00:02:04 +0100

    Added Unity Startup Project

---
+++ App/Honours Project/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public int speed = 10;
+    private Rigidbody2D playerBody;//References rigidbody applied to prefab
+    private Vector2 velocity; //x,y movement
+    private Vector2 inputMovement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        velocity = new Vector2(speed, speed);//sets velocity x and y
+        playerBody = GetComponent<Rigidbody2D>();//sets rigid body to variable
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //sets variable to user input, no input = 0
+        inputMovement = new Vector2(
+            Input.GetAxisRaw("Horizontal"),
+            Input.GetAxisRaw("Vertical")
+            );
+    }
+
+    private void FixedUpdate()//update that runs on a fixed cycle
+    {
+        Vector2 delta = inputMovement * velocity * Time.deltaTime;//takes the key being pressed * player speed * delta time(to make the movement smooth)
+        Vector2 newPosition = playerBody.position + delta;
+        playerBody.MovePosition(newPosition);
+    }
+}
