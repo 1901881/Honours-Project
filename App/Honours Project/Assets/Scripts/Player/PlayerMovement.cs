52c929682f0deb479384a502a377037385f249c8
Joseph Roper <1901881@uad.ac.uk>
2022-09-30 13:17:45 +0100

    Added a gun
    Followed this tutorial to make a gun that shoots
    
    Tutorial:
    https://www.youtube.com/watch?v=LqrAbEaDQzc

--- App/Honours Project/Assets/Scripts/Player/PlayerMovement.cs
+++ App/Honours Project/Assets/Scripts/Player/PlayerMovement.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public Weapon weapon;
+
     public int speed = 10;
     private Rigidbody2D playerBody;//References rigidbody applied to prefab
     private Vector2 velocity; //x,y movement
     private Vector2 inputMovement;
+    private Vector2 mousePosition;
 
     // Start is called before the first frame update
     void Start()
@@ -24,6 +27,12 @@ public class PlayerMovement : MonoBehaviour
             Input.GetAxisRaw("Horizontal"),
             Input.GetAxisRaw("Vertical")
             );
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            weapon.Fire();
+        }
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
     }
 
     private void FixedUpdate()//update that runs on a fixed cycle
@@ -31,5 +40,9 @@ public class PlayerMovement : MonoBehaviour
         Vector2 delta = inputMovement * velocity * Time.deltaTime;//takes the key being pressed * player speed * delta time(to make the movement smooth)
         Vector2 newPosition = playerBody.position + delta;
         playerBody.MovePosition(newPosition);
+
+        Vector2 aimDirection = mousePosition - playerBody.position;
+        float aimAngle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg - 90.0f;
+        playerBody.rotation = aimAngle;
     }
 }
