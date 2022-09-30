0c94958358095928f5d9701c80ebb92100a20022
Joseph Roper <1901881@uad.ac.uk>
2022-09-30 14:15:38 +0100

    Made a counter for bullet bounce despawn
    found in bullet script
    update git ignore to skip commiting unity libraries

--- App/Honours Project/Assets/Scripts/Player/Bullet.cs
+++ App/Honours Project/Assets/Scripts/Player/Bullet.cs
@@ -7,6 +7,8 @@ public class Bullet : MonoBehaviour
     private Rigidbody2D rb;
     Vector3 lastVelocity;
 
+    private int counter;
+
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();//sets rigid body to variable
@@ -19,11 +21,18 @@ public class Bullet : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+
         //Destroy(gameObject);
         var speed = lastVelocity.magnitude;
         var direction = Vector3.Reflect(lastVelocity.normalized, collision.contacts[0].normal);
 
         rb.velocity = direction * Mathf.Max(speed, 0.0f);
+        counter++;
 
+        if(counter >= 3)
+        {
+            Destroy(gameObject);
+            counter = 0;
+        }
     }
 }
