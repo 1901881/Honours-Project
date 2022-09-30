6d06df137c23e0f867a3c91fc3ae8869b2b88333
Joseph Roper <1901881@uad.ac.uk>
2022-09-30 13:45:25 +0100

    Added bullet bounce
    Still needs a lot of work to make it viable for the game.
    lower speed
    make it despawn after time or bounces
    
    Tutorial:
    https://www.youtube.com/watch?v=RoZG5RARGF0

--- App/Honours Project/Assets/Scripts/Player/Bullet.cs
+++ App/Honours Project/Assets/Scripts/Player/Bullet.cs
@@ -4,8 +4,26 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    Vector3 lastVelocity;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();//sets rigid body to variable
+    }
+
+    private void Update()
+    {
+        lastVelocity = rb.velocity;
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Destroy(gameObject);
+        //Destroy(gameObject);
+        var speed = lastVelocity.magnitude;
+        var direction = Vector3.Reflect(lastVelocity.normalized, collision.contacts[0].normal);
+
+        rb.velocity = direction * Mathf.Max(speed, 0.0f);
+
     }
 }
