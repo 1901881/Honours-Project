e8eaf0493f09c26ab3acfb3abee62b626890951d
Joseph Roper <1901881@uad.ac.uk>
2022-12-05 13:14:23 +0000

    Updated screen shake

--- App/Honours Project/Assets/Scripts/Player/Weapon.cs
+++ App/Honours Project/Assets/Scripts/Player/Weapon.cs
@@ -43,7 +43,7 @@ public class Weapon : MonoBehaviour
         for (int i = 0; i < bulletNum; i++)
         {
             Fire();
-            Cam.GetComponent<ScreenShake>().ShakeCam(0.08f, 0.1f);
+            Cam.GetComponent<ScreenShake>().ShakeCam(0.08f, 0.3f);
 
             yield return new WaitForSeconds(0.05f);
         }
