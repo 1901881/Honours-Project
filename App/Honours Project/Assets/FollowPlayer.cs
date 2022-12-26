ae8542e55a7481720af25486619bbc6b86131bca
Joseph Roper <1901881@uad.ac.uk>
2022-12-26 19:38:39 +0000

    Added spatialisation to the radio
    https://www.audiokinetic.com/en/library/edge/?source=Unity&id=pg_spatialaudio_preparation.html

--- App/Honours Project/Assets/FollowPlayer.cs
+++ App/Honours Project/Assets/FollowPlayer.cs
@@ -2,21 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Radio : MonoBehaviour
+public class FollowPlayer : MonoBehaviour
 {
-    /// The Wwise event to trigger a shoot sound.
-    public AK.Wwise.Event radioMusicSound = new AK.Wwise.Event();
-
-
+    public GameObject Player;
     // Start is called before the first frame update
     void Start()
     {
-      //  radioMusicSound.Post(gameObject);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        gameObject.transform.position = Player.transform.position;
     }
 }
