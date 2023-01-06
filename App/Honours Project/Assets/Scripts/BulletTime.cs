d96ff90ada97cb5bdb77957bdf26aa438ca50ad3
Joseph Roper <1901881@uad.ac.uk>
2023-01-06 17:22:58 +0000

    Added bullet time begin and end sound

--- App/Honours Project/Assets/Scripts/BulletTime.cs
+++ App/Honours Project/Assets/Scripts/BulletTime.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 public class BulletTime : MonoBehaviour
 {
     public float minTimeScale = 0.5f;
-    public float transitionSpeed = 2.0f;
+    public float transitionSpeed = 1.0f;
     public float bulletTimeDuration = 3.0f;
     public bool isBulletTime = false;
+
+    public AK.Wwise.Event EMPPlay = new AK.Wwise.Event();
+
+    [SerializeField]
+    private AK.Wwise.RTPC rtpc = null;
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +24,8 @@ public class BulletTime : MonoBehaviour
     void Update()
     {
         //set rtpc value with time scale
+        rtpc.SetValue(gameObject, Time.timeScale);
+        //rtpc.SetValue(gameObject, 0.7f);
 
         if (Input.GetKeyDown(KeyCode.LeftShift))
         {
@@ -26,21 +33,25 @@ public class BulletTime : MonoBehaviour
 
             isBulletTime = !isBulletTime;
 
-            
-            
-           
 
-            
+
+
+
+            EMPPlay.Post(gameObject);
             StartCoroutine("ToggleBulletTime");
 
 
         }
+        
     }
 
     IEnumerator ToggleBulletTime()
     {
-        string audioEvent = (isBulletTime) ? "EnterBulletTime" : "ExitBulletTime";
+        //string audioEvent = (isBulletTime) ? "EnterBulletTime" : "ExitBulletTime";
         //AkSoundEngine.PostEvent(audioEvent, this.gameObject);
+        
+        
+       
 
         float t = 0f;
         float startScale = Time.timeScale;
@@ -48,13 +59,15 @@ public class BulletTime : MonoBehaviour
         float lastTick = Time.realtimeSinceStartup;
         while(t <= 1f)
         {
+            
             t += (Time.realtimeSinceStartup - lastTick) * (1f / transitionSpeed);
             lastTick = Time.realtimeSinceStartup;
             Time.timeScale = Mathf.Lerp(startScale, targetScale, t);
             Time.fixedDeltaTime = 0.02f * Time.timeScale;
-            yield return new WaitForSeconds(bulletTimeDuration);
+            yield return null;
+/*            yield return new WaitForSeconds(bulletTimeDuration);
             isBulletTime = !isBulletTime;
-            StartCoroutine("ToggleBulletTime");
+            StartCoroutine("ToggleBulletTime");*/
         }
     }
 }
