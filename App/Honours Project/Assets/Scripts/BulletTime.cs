d4c5d854ed875d752c7eb27a4d4677f08dfcad53
Joseph Roper <1901881@uad.ac.uk>
2023-01-06 03:07:31 +0000

    Added bullet time effect
    still need to add audio and visuals for it
    
    https://www.youtube.com/watch?v=_UkUl4fFX-A&ab_channel=DerekStobbe
    https://www.youtube.com/watch?v=WdSfbf_gFPk&ab_channel=LampEightAudio

---
+++ App/Honours Project/Assets/Scripts/BulletTime.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletTime : MonoBehaviour
+{
+    public float minTimeScale = 0.5f;
+    public float transitionSpeed = 2.0f;
+    public float bulletTimeDuration = 3.0f;
+    public bool isBulletTime = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //set rtpc value with time scale
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            Debug.Log("Left shift hit");
+
+            isBulletTime = !isBulletTime;
+
+            
+            
+           
+
+            
+            StartCoroutine("ToggleBulletTime");
+
+
+        }
+    }
+
+    IEnumerator ToggleBulletTime()
+    {
+        string audioEvent = (isBulletTime) ? "EnterBulletTime" : "ExitBulletTime";
+        //AkSoundEngine.PostEvent(audioEvent, this.gameObject);
+
+        float t = 0f;
+        float startScale = Time.timeScale;
+        float targetScale = (isBulletTime) ? minTimeScale : 1;
+        float lastTick = Time.realtimeSinceStartup;
+        while(t <= 1f)
+        {
+            t += (Time.realtimeSinceStartup - lastTick) * (1f / transitionSpeed);
+            lastTick = Time.realtimeSinceStartup;
+            Time.timeScale = Mathf.Lerp(startScale, targetScale, t);
+            Time.fixedDeltaTime = 0.02f * Time.timeScale;
+            yield return new WaitForSeconds(bulletTimeDuration);
+            isBulletTime = !isBulletTime;
+            StartCoroutine("ToggleBulletTime");
+        }
+    }
+}
