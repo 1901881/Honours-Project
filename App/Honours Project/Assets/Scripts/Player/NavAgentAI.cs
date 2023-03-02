a732e10ecfa50b0eec487b585e119180fc10be65
Joseph Roper <1901881@uad.ac.uk>
2023-03-02 15:43:13 +0000

    Got basic enemy working in behaviour trees
    need to adjust wander values

--- App/Honours Project/Assets/Scripts/Player/NavAgentAI.cs
+++ App/Honours Project/Assets/Scripts/Player/NavAgentAI.cs
@@ -2,15 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using BehaviorDesigner.Runtime;
+using static UnityEngine.Rendering.DebugUI;
 
 public class NavAgentAI : MonoBehaviour
 {
-    
+    public float seekSpeed;
+    public float seekViewAngle;
+    public float seekViewDistance;
+
+    public float pursueSpeed;
+    public float pursueViewAngle;
+    public float pursueViewDistance;
+
+    public float attackSpeed;
+    public float attackViewAngle;
+    public float attackViewDistance;
+
+    public BehaviorTree behaviorTree;
 
     // Start is called before the first frame update
     void Start()
     {
+/*        behaviorTree.SetVariableValue("seekSpeed", seekSpeed);
+        behaviorTree.SetVariableValue("seekViewAngle", seekViewAngle);
+        behaviorTree.SetVariableValue("seekViewDistance", seekViewDistance);
 
+        behaviorTree.SetVariableValue("pursueSpeed", pursueSpeed);
+        behaviorTree.SetVariableValue("pursueViewAngle", pursueViewAngle);
+        behaviorTree.SetVariableValue("pursueViewDistance", pursueViewDistance);
+
+        behaviorTree.SetVariableValue("attackSpeed", attackSpeed);
+        behaviorTree.SetVariableValue("attackViewAngle", attackViewAngle);
+        behaviorTree.SetVariableValue("attackViewDistance", attackViewDistance);*/
     }
 
     // Update is called once per frame
