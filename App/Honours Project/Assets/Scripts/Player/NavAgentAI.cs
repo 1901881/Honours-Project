f2856e2569f65c9ced094a780e1561b211139fe2
Joseph Roper <1901881@uad.ac.uk>
2023-03-02 16:15:00 +0000

    Added NPC attack and death

--- App/Honours Project/Assets/Scripts/Player/NavAgentAI.cs
+++ App/Honours Project/Assets/Scripts/Player/NavAgentAI.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.AI;
 using BehaviorDesigner.Runtime;
 using static UnityEngine.Rendering.DebugUI;
+using UnityEditor;
 
 public class NavAgentAI : MonoBehaviour
 {
@@ -21,20 +22,33 @@ public class NavAgentAI : MonoBehaviour
 
     public BehaviorTree behaviorTree;
 
+
+    //Changing Enemy Color
+    private SpriteRenderer SpriteRend;
+    private Color originalColor;
+
+
+    public int health = 3;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-/*        behaviorTree.SetVariableValue("seekSpeed", seekSpeed);
-        behaviorTree.SetVariableValue("seekViewAngle", seekViewAngle);
-        behaviorTree.SetVariableValue("seekViewDistance", seekViewDistance);
+        /*        behaviorTree.SetVariableValue("seekSpeed", seekSpeed);
+                behaviorTree.SetVariableValue("seekViewAngle", seekViewAngle);
+                behaviorTree.SetVariableValue("seekViewDistance", seekViewDistance);
 
-        behaviorTree.SetVariableValue("pursueSpeed", pursueSpeed);
-        behaviorTree.SetVariableValue("pursueViewAngle", pursueViewAngle);
-        behaviorTree.SetVariableValue("pursueViewDistance", pursueViewDistance);
+                behaviorTree.SetVariableValue("pursueSpeed", pursueSpeed);
+                behaviorTree.SetVariableValue("pursueViewAngle", pursueViewAngle);
+                behaviorTree.SetVariableValue("pursueViewDistance", pursueViewDistance);
 
-        behaviorTree.SetVariableValue("attackSpeed", attackSpeed);
-        behaviorTree.SetVariableValue("attackViewAngle", attackViewAngle);
-        behaviorTree.SetVariableValue("attackViewDistance", attackViewDistance);*/
+                behaviorTree.SetVariableValue("attackSpeed", attackSpeed);
+                behaviorTree.SetVariableValue("attackViewAngle", attackViewAngle);
+                behaviorTree.SetVariableValue("attackViewDistance", attackViewDistance);*/
+
+        SpriteRend = GetComponent<SpriteRenderer>();
+        originalColor = SpriteRend.color;
     }
 
     // Update is called once per frame
@@ -42,6 +56,36 @@ public class NavAgentAI : MonoBehaviour
     {
         transform.SetPositionAndRotation(transform.position, new Quaternion( 0f, 0f, transform.rotation.z, transform.rotation.w));
         //transform.rotation = Quaternion.Euler(0, 0, transform.rotation.eulerAngles.z);
+
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Bullet")
+        {
+            StartCoroutine(Hit());
+        }
+        if (collision.gameObject.tag == "Player")
+        {
+            StartCoroutine(collision.gameObject.GetComponent<PlayerMovement>().Hit());
+            Debug.Log("hit player");
+            //StartCoroutine(AttackPause());
+        }
+    }
+
+    IEnumerator Hit()
+    {
+        SpriteRend.color = Color.white;
+        Time.timeScale = 0;
+        yield return new WaitForSecondsRealtime(0.08f);
+        Time.timeScale = 1;
+        SpriteRend.color = originalColor;
+        health--;
+
     }
 }
 
