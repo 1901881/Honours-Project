3fbfc7c66a5b91394ddc55cff6b7cba43ecd9b31
Joseph Roper <1901881@uad.ac.uk>
2023-03-08 13:20:53 +0000

    Made a new level for testing
    added 3d walls
    started work of the stress calculator

--- App/Honours Project/Assets/Scripts/Player/NavAgentAI.cs
+++ App/Honours Project/Assets/Scripts/Player/NavAgentAI.cs
@@ -5,6 +5,9 @@ using UnityEngine.AI;
 using BehaviorDesigner.Runtime;
 using static UnityEngine.Rendering.DebugUI;
 using UnityEditor;
+using static UnityEngine.GraphicsBuffer;
+using System;
+using System.Linq;
 
 public class NavAgentAI : MonoBehaviour
 {
@@ -30,23 +33,14 @@ public class NavAgentAI : MonoBehaviour
 
     public int health = 3;
 
+    public int stressResponse;
+    public float stressValue;
 
+    private Vector3 target;
 
     // Start is called before the first frame update
     void Start()
     {
-        /*        behaviorTree.SetVariableValue("seekSpeed", seekSpeed);
-                behaviorTree.SetVariableValue("seekViewAngle", seekViewAngle);
-                behaviorTree.SetVariableValue("seekViewDistance", seekViewDistance);
-
-                behaviorTree.SetVariableValue("pursueSpeed", pursueSpeed);
-                behaviorTree.SetVariableValue("pursueViewAngle", pursueViewAngle);
-                behaviorTree.SetVariableValue("pursueViewDistance", pursueViewDistance);
-
-                behaviorTree.SetVariableValue("attackSpeed", attackSpeed);
-                behaviorTree.SetVariableValue("attackViewAngle", attackViewAngle);
-                behaviorTree.SetVariableValue("attackViewDistance", attackViewDistance);*/
-
         SpriteRend = GetComponent<SpriteRenderer>();
         originalColor = SpriteRend.color;
     }
@@ -61,6 +55,8 @@ public class NavAgentAI : MonoBehaviour
         {
             Destroy(gameObject);
         }
+
+        CalculateStress();
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -94,13 +90,86 @@ public class NavAgentAI : MonoBehaviour
         UnityEditor.Handles.DrawWireDisc(transform.position, transform.forward, fleeRadius);
     }
 
+    void CalculateStress()
+    {
+        /*     bool healthStress = false;
+             if(health <= 1 && !healthStress)
+             {
+                 stressValue += 10;
+                 healthStress = true;
+             }*/
+ /*       target = GameObject.FindWithTag("Player").transform.position;
+        float maxDistance = 10.0f;
+        float distanceToTarget = Vector3.Distance(transform.position, target);
+        if (distanceToTarget > maxDistance)
+        {
+            distanceToTarget = 1;//make it null
+        }*/
+        //stress value/
+        // x = health * distance to player * bullets near player * stress likeliness
+
+        //maybe cap the range of distance
+        //reverse health
+
+        //stressValue = health * distanceToTarget;
+        //Debug.Log(stressValue);
+        //Debug.Log(distanceToTarget);
+
+        float healthFactor = 1;
+        float distanceFactor = 1;
+        float bulletFactor = 1;
+        float hitFactor = 1;
+
+        //just do adds?, maybe speak to salma about this
+
+
+
+        float stressFortitude = 0;
+        float stressValue = 0;
+
+        stressValue = healthFactor * distanceFactor * bulletFactor * hitFactor;
+
+        if(stressValue >= stressFortitude)
+        {
+            float fightWeighting = 0;
+            float flightWeighting = 0;
+            float freezeWeighting = 0;
+
+            float[] stressWeightings = { fightWeighting, flightWeighting, freezeWeighting};
+            int[] stressCounter = new int[3];//fight, flight, freeze
+
+            int testAmount = 5;
+
+            for (int i = 0; i < stressWeightings.Length; i++)
+            {
+                for (int x = 0; x < testAmount; x++)
+                {
+                    float randomNumber = 0; //1-100
+                    if (randomNumber <= stressWeightings[i])
+                    {
+                        stressCounter[i]++;
+                    }
+                }
+            }
+
+            //check which one is bigger, then return the case
+            int maxIndex = Array.IndexOf(stressCounter, stressCounter.Max());
+
+            switch(maxIndex)
+            {
+                case 0:
+                    Debug.Log("fight");
+                    break;
+                case 1:
+                    Debug.Log("flight");
+                    break;
+                case 2:
+                    Debug.Log("freeze");
+                    break;
+            }
+
+        }
+
+       
+    }
 }
-
-/*
-
-//public NavMeshAgent agent;
-//agent = GetComponent<NavMeshAgent>();
-// agent.updateUpAxis = false;
-//agent.updateRotation = false;
-
- */
