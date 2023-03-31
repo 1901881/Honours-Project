d8313e2bb546df4b3c38215c31ca8f5d38f9229d
Joseph Roper <1901881@uad.ac.uk>
2023-03-31 21:23:57 +0100

    Added suprised emoticon for when NPC sees player
    needs testing

--- App/Honours Project/Assets/Scripts/NPCEmoticonSwitcher.cs
+++ App/Honours Project/Assets/Scripts/NPCEmoticonSwitcher.cs
@@ -16,6 +16,8 @@ public class NPCEmoticonSwitcher : MonoBehaviour
     public Color fawnColour;
     public Sprite neutralSprite;
     public Color neutralColour;
+    public Sprite suprisedSprite;
+    public Color suprisedColour;
 
     public GameObject NPC;
     public GameObject faceVisor;
@@ -31,28 +33,32 @@ public class NPCEmoticonSwitcher : MonoBehaviour
         switch (faceNum)
         {
             case 0:
-                NPC.gameObject.GetComponent<SpriteRenderer>().sprite = neutralSprite;
-                faceVisor.gameObject.GetComponent<SpriteRenderer>().color = neutralColour;
-                break;
-            case 1:
                 NPC.gameObject.GetComponent<SpriteRenderer>().sprite = fightSprite;
                 faceVisor.gameObject.GetComponent<SpriteRenderer>().color = fightColour;
                 break;
-            case 2:
+            case 1:
                 NPC.gameObject.GetComponent<SpriteRenderer>().sprite = flightSprite;
                 faceVisor.gameObject.GetComponent<SpriteRenderer>().color = flightColour;
                 break;
-            case 3:
+            case 2:
                 NPC.gameObject.GetComponent<SpriteRenderer>().sprite = freezeSprite;
                 faceVisor.gameObject.GetComponent<SpriteRenderer>().color = freezeColour;
                 break;
-            case 4:
+            case 3:
                 NPC.gameObject.GetComponent<SpriteRenderer>().sprite = flopSprite;
                 faceVisor.gameObject.GetComponent<SpriteRenderer>().color = flopColour;
                 break;
-            case 5:
+            case 4:
                 NPC.gameObject.GetComponent<SpriteRenderer>().sprite = fawnSprite;
                 faceVisor.gameObject.GetComponent<SpriteRenderer>().color = fawnColour;
+                break;
+            case 5:
+                NPC.gameObject.GetComponent<SpriteRenderer>().sprite = neutralSprite;
+                faceVisor.gameObject.GetComponent<SpriteRenderer>().color = neutralColour;
+                break;
+            case 6:
+                NPC.gameObject.GetComponent<SpriteRenderer>().sprite = suprisedSprite;
+                faceVisor.gameObject.GetComponent<SpriteRenderer>().color = suprisedColour;
                 break;
             default:
                 NPC.gameObject.GetComponent<SpriteRenderer>().sprite = neutralSprite;
