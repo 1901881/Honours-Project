427dedfc7c5cd72b8f5953cf35af1afd0cb328a8
Joseph Roper <1901881@uad.ac.uk>
2023-04-05 13:10:02 +0100

    Got stress weighting sliders on inspector window

--- App/Honours Project/Assets/Scripts/NPCEditor.cs
+++ App/Honours Project/Assets/Scripts/NPCEditor.cs
@@ -4,36 +4,67 @@ using UnityEngine;
 [CustomEditor(typeof(NavAgentAI))]
 public class NPCEditor : Editor
 {
-    bool showPosition = true;
-    string status = "Select a GameObject";
+    bool showStressCalculation = true;
+    bool showStressWeightings = true;
+
+    NavAgentAI navAgentAI;
 
     [MenuItem("Examples/Foldout Usage")]
     static void Init()
     {
-       /* NPCEditor window = (NPCEditor)GetWindow(typeof(NPCEditor));
-        window.Show();*/
+        /* NPCEditor window = (NPCEditor)GetWindow(typeof(NPCEditor));
+         window.Show();*/
     }
 
     public override void OnInspectorGUI()
     {
-        showPosition = EditorGUILayout.Foldout(showPosition, status);
-        if (showPosition)
-            if (Selection.activeTransform)
-            {
-                Selection.activeTransform.position =
-                    EditorGUILayout.Vector3Field("Position", Selection.activeTransform.position);
-                status = Selection.activeTransform.name;
-            }
+        navAgentAI = (NavAgentAI)target;
 
-        if (!Selection.activeTransform)
-        {
-            status = "Select a GameObject";
-            showPosition = false;
-        }
+        StressWeightingsUpdate();
+        StressCalculationUpdate();
     }
 
     public void OnInspectorUpdate()
     {
         this.Repaint();
+    }
+
+    private void StressWeightingsUpdate()
+    {
+        showStressWeightings = EditorGUILayout.Foldout(showStressWeightings, "Stress Weightings");
+        if (showStressWeightings)
+            if (Selection.activeTransform)
+            {
+                navAgentAI.fightWeighting = EditorGUILayout.Slider("Fight Weighting", navAgentAI.fightWeighting, 0, 100);
+                navAgentAI.flightWeighting = EditorGUILayout.Slider("Flight Weighting", navAgentAI.flightWeighting, 0, 100);
+                navAgentAI.freezeWeighting = EditorGUILayout.Slider("Freeze Weighting", navAgentAI.freezeWeighting, 0, 100);
+                navAgentAI.flopWeighting = EditorGUILayout.Slider("Flop Weighting", navAgentAI.flopWeighting, 0, 100);
+                navAgentAI.fawnWeighting = EditorGUILayout.Slider("Fawn Weighting", navAgentAI.fawnWeighting, 0, 100);
+
+                //learn how to just show a variable
+            }
+
+        if (!Selection.activeTransform)
+        {
+            showStressWeightings = false;
+        }
+    }
+
+    private void StressCalculationUpdate()
+    {
+        showStressCalculation = EditorGUILayout.Foldout(showStressCalculation, "Stress Calculation");
+        if (showStressCalculation)
+            if (Selection.activeTransform)
+            {
+                Selection.activeTransform.position =
+                    EditorGUILayout.Vector3Field("Position", Selection.activeTransform.position);
+
+
+            }
+        
+        if (!Selection.activeTransform)
+        {
+            showStressCalculation = false;
+        }
     }
 }
