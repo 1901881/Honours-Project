426b93eaddb937ef28866f7644f0724165132b73
Joseph Roper <1901881@uad.ac.uk>
2023-04-15 10:19:15 +0100

    Added level buttons
    Added level buttons, appear when menu button hit
    Need to change the colour of the new buttons i used the same profile but i gues its just a difference between computers, change on desktop
    
    ToDo:
    now add the level switcher for when buttons get hit

--- App/Honours Project/Assets/Scripts/MainMenu.cs
+++ App/Honours Project/Assets/Scripts/MainMenu.cs
@@ -1,36 +1,107 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Burst.CompilerServices;
 using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
     public Camera menuCam;
+    Transform[] allChildren;
+    private void Start()
+    {
+        allChildren = GetComponentsInChildren<Transform>();
+        foreach (Transform child in allChildren)
+        {
+            if(child.tag == "Numbers")
+            {
+                child.gameObject.SetActive(false);
+            }
+            
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            RaycastHit2D hit = Physics2D.GetRayIntersection(Camera.main.ScreenPointToRay(Input.mousePosition));
+            RaycastHit2D hit = Physics2D.GetRayIntersection(menuCam.ScreenPointToRay(Input.mousePosition));
 
             if (hit.collider != null)
             {
-                if (hit.collider.gameObject.name == "MenuButton")
-                {
-                    Debug.Log("Hit Menu Button");
-                }
+                MenuButtonUpdate(hit);
+                LevelButtonUpdate(hit);
 
-                if (hit.collider.gameObject.name == "PlayButton")
-                {
-                    Debug.Log("Hit Play Button");
-                }
-
-                if (hit.collider.gameObject.name == "QuitButton")
-                {
-                    Debug.Log("Hit Quit Button");
-                }
-                Debug.Log("Target Position: " + hit.collider.gameObject.transform.position);
+                
+                //Debug.Log("Target Position: " + hit.collider.gameObject.transform.position);
             }
             
         } 
     }
+
+    void MenuButtonUpdate(RaycastHit2D hit)
+    {
+        if (hit.collider.gameObject.name == "MenuButton")
+        {
+            Debug.Log("Hit Menu Button");
+            foreach (Transform child in allChildren)
+            {
+                if (child.tag == "MenuButtons")
+                {
+                    child.gameObject.SetActive(false);
+                }
+                if (child.tag == "Numbers")
+                {
+                    child.gameObject.SetActive(true);
+                }
+            }
+        }
+
+        if (hit.collider.gameObject.name == "PlayButton")
+        {
+            Debug.Log("Hit Play Button");
+        }
+
+        if (hit.collider.gameObject.name == "QuitButton")
+        {
+            Debug.Log("Hit Quit Button");
+        }
+    }
+
+    void LevelButtonUpdate(RaycastHit2D hit)
+    {
+        if (hit.collider.gameObject.name == "BackArrow")
+        {
+            Debug.Log("Hit Back Arrow");
+            foreach (Transform child in allChildren)
+            {
+                if (child.tag == "MenuButtons")
+                {
+                    child.gameObject.SetActive(true);
+                }
+                if (child.tag == "Numbers")
+                {
+                    child.gameObject.SetActive(false);
+                }
+            }
+        }
+
+        //Levels
+        if (hit.collider.gameObject.name == "1")
+        {
+            Debug.Log("Load Level 1");
+        }
+    }
 }
+/*
+ To DO:
+
+    get children of game object camera
+
+    get children of gameobject, numbers, buttons
+    turn off numbers on start
+
+need a back button
+    
+ */
+
