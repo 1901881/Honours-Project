c67d071d0d105c58f9d2b54e47ea1c2aace1f544
Joseph Roper <1901881@uad.ac.uk>
2023-04-20 17:57:45 +0100

    Updated menu for different tests

--- App/Honours Project/Assets/Scripts/MainMenu.cs
+++ App/Honours Project/Assets/Scripts/MainMenu.cs
@@ -8,16 +8,24 @@ public class MainMenu : MonoBehaviour
 {
     public Camera menuCam;
     Transform[] allChildren;
+
+    private bool test1Update = false;
+    private bool test2Update = false;
+
     private void Start()
     {
         allChildren = GetComponentsInChildren<Transform>();
         foreach (Transform child in allChildren)
         {
-            if(child.tag == "Numbers")
+            if(child.tag == "Test1Numbers" || child.tag == "Test2Numbers" || child.tag == "Test1Buttons" || child.tag == "Test2Buttons")//|| child.tag == "MenuButtons"
             {
-                child.gameObject.SetActive(false);
+                child.gameObject.SetActive(false); 
             }
-            
+            if(child.name == "Test 1" || child.name == "Test 2")
+            {
+                child.gameObject.SetActive(true);
+            }
+
         }
     }
 
@@ -30,28 +38,71 @@ public class MainMenu : MonoBehaviour
 
             if (hit.collider != null)
             {
-                MenuButtonUpdate(hit);
-                LevelButtonUpdate(hit);
+                if (hit.collider.gameObject.name == "Test1")
+                {
+                    test1Update = true;
+                    Debug.Log(hit.collider.gameObject.name.ToString());
+                    foreach (Transform child in allChildren)
+                    {
+                        //hit.collider.gameObject.SetActive(false);
+                        if (child.tag == "Test1Buttons")
+                        {
+                            child.gameObject.SetActive(true);
+                        }
+                        if (child.gameObject.name == "Test1" || child.gameObject.name == "Test2")
+                        {
+                            child.gameObject.SetActive(false);
+                        }
 
-                
-                //Debug.Log("Target Position: " + hit.collider.gameObject.transform.position);
+                    }
+                }
+
+                if (hit.collider.gameObject.name == "Test2")
+                {
+                    test2Update = true;
+                    Debug.Log(hit.collider.gameObject.name.ToString());
+                    foreach (Transform child in allChildren)
+                    {
+                        //hit.collider.gameObject.SetActive(false);
+                        if (child.tag == "Test2Buttons")
+                        {
+                            child.gameObject.SetActive(true);
+                        }
+                        if (child.gameObject.name == "Test1" || child.gameObject.name == "Test2")
+                        {
+                            child.gameObject.SetActive(false);
+                        }
+
+                    }
+                }
+
+                if (test1Update)
+                {
+                    MenuButtonUpdate(hit, 2, "Test1Buttons", "Test1Numbers");
+                    LevelButtonUpdate(hit, "Test1Buttons", "Test1Numbers", 1, 2, 3, 4, 5);
+                }
+                if (test2Update)
+                {
+                    MenuButtonUpdate(hit, 2, "Test2Buttons", "Test2Numbers");
+                    LevelButtonUpdate(hit, "Test2Buttons", "Test2Numbers", 1, 2, 3, 4, 5);
+                }
             }
             
         } 
     }
 
-    void MenuButtonUpdate(RaycastHit2D hit)
+    void MenuButtonUpdate(RaycastHit2D hit, int firstLevel, string testButtonsTag, string testNumbersTag)
     {
         if (hit.collider.gameObject.name == "MenuButton")
         {
             Debug.Log("Hit Menu Button");
             foreach (Transform child in allChildren)
             {
-                if (child.tag == "MenuButtons")
+                if (child.tag == testButtonsTag)
                 {
                     child.gameObject.SetActive(false);
                 }
-                if (child.tag == "Numbers")
+                if (child.tag == testNumbersTag)
                 {
                     child.gameObject.SetActive(true);
                 }
@@ -61,29 +112,51 @@ public class MainMenu : MonoBehaviour
         if (hit.collider.gameObject.name == "PlayButton")
         {
             Debug.Log("Hit Play Button");
-            SceneManager.LoadScene(2);
+            SceneManager.LoadScene(firstLevel);
         }
 
         if (hit.collider.gameObject.name == "QuitButton")
         {
             Debug.Log("Hit Quit Button");
+            #if UNITY_EDITOR
+                        UnityEditor.EditorApplication.isPlaying = false;
+            #endif
+                        Application.Quit();
         }
-    }
 
-    void LevelButtonUpdate(RaycastHit2D hit)
-    {
         if (hit.collider.gameObject.name == "BackArrow")
         {
             Debug.Log("Hit Back Arrow");
             foreach (Transform child in allChildren)
             {
-                if (child.tag == "MenuButtons")
-                {
-                    child.gameObject.SetActive(true);
-                }
-                if (child.tag == "Numbers")
+                if (child.tag == testButtonsTag)
                 {
                     child.gameObject.SetActive(false);
+                }
+                if (child.tag == testNumbersTag)
+                {
+                    child.gameObject.SetActive(false);
+                }
+            }
+        }
+    }
+
+    void LevelButtonUpdate(RaycastHit2D hit, string testButtonsTag, string testNumbersTag, int level1, int level2, int level3, int level4, int level5)
+    {
+        if (hit.collider.gameObject.name == "BackArrow")
+        {
+            test1Update = false;
+            test2Update = false;
+            Debug.Log("Hit Back Arrow");
+            foreach (Transform child in allChildren)
+            {
+                if (child.tag == testButtonsTag)
+                {
+                    child.gameObject.SetActive(false);
+                }
+                if (child.gameObject.name == "Test1" || child.gameObject.name == "Test2")
+                {
+                    child.gameObject.SetActive(true);
                 }
             }
         }
@@ -93,33 +166,35 @@ public class MainMenu : MonoBehaviour
         {
             Debug.Log("Load Level 1");
             //Lvl_1_StressBot
-            SceneManager.LoadScene("Lvl_1_StressBot");
+            SceneManager.LoadScene(level1);
         }
         if (hit.collider.gameObject.name == "2")
         {
             Debug.Log("Load Level 2");
             //Lvl_1_StressBot
-            SceneManager.LoadScene("Lvl_2_StressBot");
+            SceneManager.LoadScene(level2);
         }
         if (hit.collider.gameObject.name == "3")
         {
             Debug.Log("Load Level 3");
             //Lvl_1_StressBot
-            SceneManager.LoadScene("Lvl_3_StressBot");
+            SceneManager.LoadScene(level3);
         }
         if (hit.collider.gameObject.name == "4")
         {
             Debug.Log("Load Level 4");
             //Lvl_1_StressBot
-            SceneManager.LoadScene("Lvl_4_StressBot");
+            SceneManager.LoadScene(level4);
         }
         if (hit.collider.gameObject.name == "5")
         {
             Debug.Log("Load Level 5");
             //Lvl_1_StressBot
-            SceneManager.LoadScene("Lvl_5_StressBot");
+            SceneManager.LoadScene(level5);
         }
     }
+
+
 }
 /*
  To DO:
