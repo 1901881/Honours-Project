52cdcad970a6c6af4247dec7203391eba001f5dc
Joseph Roper <1901881@uad.ac.uk>
2023-04-25 19:34:12 +0100

    Updated enemy counter

--- App/Honours Project/Assets/Scripts/UI/EnemyCounter.cs
+++ App/Honours Project/Assets/Scripts/UI/EnemyCounter.cs
@@ -29,11 +29,7 @@ public class EnemyCounter : MonoBehaviour
 
         if(enemies.Length == 0)
         {
-            if(SceneManager.GetActiveScene().name == "Lvl_5_StressBot")
-            {
-                StartCoroutine(GameObject.FindObjectOfType<HUDManager>().YouWin());
-            }
-            if (SceneManager.GetActiveScene().name == "Lvl_5_BasicBot")
+            if(SceneManager.GetActiveScene().name == "Lvl_5_StressBot" || SceneManager.GetActiveScene().name == "Lvl_5_BasicBot")
             {
                 StartCoroutine(GameObject.FindObjectOfType<HUDManager>().YouWin());
             }
